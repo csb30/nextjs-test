5af60d9ed4eb3d71eb39683c43e5eb60e95d3b20
csb30 <30balint@gmail.com>
2025-02-07 17:47:14 +0100

    changed fonts

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
         <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={70} height={70} />
+                    <Image src="/logo1.png" alt="logo" width={144} height={30} />
                 </Link>
                 <div className='flex items-center gap-5 text-black'>
                     {session && session?.user ? (
